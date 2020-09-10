30addcf099fae83aad6e8fb3233ab61054b1ae78
Tim Repke <tim@repke.eu>
2020-09-10 16:37:07 +0200

    add umap

---
+++ Code/data_generators/__init__.py
@@ -0,0 +1,8 @@
+import random
+from typing import Union, Dict, Iterable
+
+
+def temporal(intervals: Union[int, Dict[Union[int, str], float]],
+             label_dist: Iterable[Dict[Union[int, str], float]],
+             replace=False):
+    pass
